1dde7b5d68e28a5e846c071358e3b809fd67212f
Rahul <rahul39khanal@gmail.com>
2023-07-01 16:42:39 +0545

    server connected

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 const app = express();
 
 app.use(express.json());
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 7000;
+
+const start = async () => {
+  try {
+    app.listen(PORT, console.log(`connected to server ${PORT}....`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
